9fa91792a3d81f83cebb7495e4f0738ab542e220
hungrypanda140 <trevor_ing+cs1300@brown.edu>
2022-12-07 21:41:08 -0500

    Fixing URL paths

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<NextUIProvider theme={darkTheme}>
-			<BrowserRouter>
+			<BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
 				<ScrollToTop />
 				<App />
 			</BrowserRouter>
